93c8805035fcdacb5cc0fd10089cdda88c524e06
bizzyizdizzy@gmail.com <bizzyizdizzy@gmail.com@7fa0ba4a-8dc7-7c1d-e73d-892634f7c85f>
2011-06-11 12:54:44 +0000

    make script changes

---
+++ codename-m-and-m/make.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+# Flags for compiling sources
+NASMFLAGS=""
+CPPFLAGS=""
+CFLAGS=""
+# Sources for compiling
+NASMSRC=""
+CPPSRC=""
+CSRC=""
+function compile(){
+	echo "Compiling!"
+}
+function assemble(){
+	echo "Assembling!"
+}
+function link(){
+	echo "Linking!"
+}
+function help(){
+	echo "Help!"
+	exit 0;
+}
+
+case $1 in
+	(help|h)
+		help;;
+	(compile)
+		compile;;
+	(assemble)
+		assemble;;
+	(link)
+		link;;
+	(all)
+		assemble;
+		compile;
+		link;;
+	(*)
+		help;;
