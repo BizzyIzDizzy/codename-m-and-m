bc95fe30d599447b457e49ec9962066283d91202
bizzyizdizzy@gmail.com <bizzyizdizzy@gmail.com@7fa0ba4a-8dc7-7c1d-e73d-892634f7c85f>
2011-06-11 13:54:00 +0000

    added object folder

--- codename-m-and-m/make.sh
+++ codename-m-and-m/make.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Files with sources
-NASMF="asm"
-CPPF="src"
-CF="src"
+NASMF="./asm/"
+CPPF="./src/"
+CF="./src/"
 # Flags for compiling and linking
-NASMFLAGS=""
+NASMFLAGS="-f aout -o "
 CPPFLAGS=""
 CFLAGS=""
 LDFLAGS=""
 # Sources for compiling
-NASMSRC="boot.asm kernel.asm test.asm"
+NASMSRC="boot.asm"
 CPPSRC=""
 CSRC=""
 # Linker script
@@ -17,7 +17,14 @@ LDSCRIPT="LinkerScript.ld"
 
 function Assemble(){
 	while((${#1}!=0)); do
-		
+		printf "\n"
+		input=$1
+		output="${1%.asm}.o"
+		echo "Assembling file \"$input\"!"
+		nasm $NASMFLAGS $output $NASMF$input || printf "Assembling failed! Check the file for errors!\n\n"
+		if [ -a $output ]; then
+			printf "Assembling was a success! Output file = $output!\n\n"
+		fi
 		shift
 	done
 
@@ -41,7 +48,7 @@ case $1 in
 	(link)
 		Link;;
 	(all)
-		Assemble;
+		Assemble $NASMSRC;
 		Compile;
 		Link;;
 	(*)
