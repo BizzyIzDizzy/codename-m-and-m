ac593838be8d2e425be69fc4ccebdee1bdb49ce3
bizzyizdizzy@gmail.com <bizzyizdizzy@gmail.com@7fa0ba4a-8dc7-7c1d-e73d-892634f7c85f>
2011-06-11 13:33:28 +0000

    adding asm folder for asm sources

--- codename-m-and-m/make.sh
+++ codename-m-and-m/make.sh
@@ -1,3 +1,50 @@
 #!/bin/bash
-echo "testing"
-exit 0 
+# Files with sources
+NASMF="asm"
+CPPF="src"
+CF="src"
+# Flags for compiling and linking
+NASMFLAGS=""
+CPPFLAGS=""
+CFLAGS=""
+LDFLAGS=""
+# Sources for compiling
+NASMSRC="boot.asm kernel.asm test.asm"
+CPPSRC=""
+CSRC=""
+# Linker script
+LDSCRIPT="LinkerScript.ld"
+
+function Assemble(){
+	while((${#1}!=0)); do
+		
+		shift
+	done
+
+}
+function Compile(){
+	echo "Compiling!"
+}
+function Link(){
+	echo "Linking!"
+}
+function Help(){
+	echo "Help!"
+}
+case $1 in
+	(help|h)
+		Help;;
+	(assemble)
+		Assemble $NASMSRC;;
+	(compile)
+		Compile;;
+	(link)
+		Link;;
+	(all)
+		Assemble;
+		Compile;
+		Link;;
+	(*)
+		Help;;
+esac
+exit 0
